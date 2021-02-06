67ecf307e0a17f748ddc210c2539e4360834763a
Chris McKinney <mmycodetuts@gmail.com>
2021-02-06 08:00:38 -0600

    Created an empty, bare-bones project - mmy

---
+++ src/main/kotlin/com/mmycodetuts/BloqBuster/BloqBusterApplication.kt
@@ -0,0 +1,11 @@
+package com.mmycodetuts.BloqBuster
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BloqBusterApplication
+
+fun main(args: Array<String>) {
+	runApplication<BloqBusterApplication>(*args)
+}
---
+++ src/test/kotlin/com/mmycodetuts/BloqBuster/BloqBusterApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mmycodetuts.BloqBuster
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BloqBusterApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
