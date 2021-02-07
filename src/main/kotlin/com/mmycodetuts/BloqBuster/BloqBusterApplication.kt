b3c5c50b33f1c76ed261e2b54de081073ddc4b4b
Chris McKinney <mmycodetuts@gmail.com>
2021-02-06 21:52:39 -0600

    Revert "renamed base package - mmy"
    This reverts commit 562672e1

--- src/main/kotlin/com/mmycodetuts/BloqBuster/BloqBusterApplication.kt
+++ src/main/kotlin/com/mmycodetuts/BloqBuster/BloqBusterApplication.kt
@@ -1,4 +1,4 @@
-package com.mmycodetuts.bloqBuster
+package com.mmycodetuts.BloqBuster
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
